e47a04e9019a1a0f86a4426ba7581f2f8f30629e
VictoriaSzy <vszymanowski00@stuy.edu>
2019-02-26 11:37:09 -0500

    Successfully printed out each line of the text by using new file, scanner, and while loop

--- ReadFile.java
+++ ReadFile.java
@@ -3,5 +3,19 @@ import java.io.FileNotFoundException ;
 import java.util.Scanner ;
 
 public class ReadFile {
-
+  public static void main(String[] args) {
+    try {
+      File f = new File("Maze1.txt") ;
+      Scanner s = new Scanner(f) ;
+      while (s.hasNextLine()) {
+        String line = s.nextLine() ;
+        System.out.println(line) ;
+      }
+    } catch (FileNotFoundException e) {
+      System.out.println("The file was not found!") ;
+    }
+    catch (Exception e) {
+      System.out.println("There is a strange exception that was triggered!") ;
+    }
+  }
 }
